acf1a752a242a050112311dc96611e2bd49d2ae1
Lains <lainsce@airmail.cc>
2017-06-25 17:01:40 -0300

    1.1.4 - Make the meson/post_install.py file more secure

--- meson/post_install.py
+++ meson/post_install.py
@@ -7,4 +7,4 @@ schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0'
 
 if not os.environ.get('DESTDIR'):
 	print('Compiling gsettings schemas...')
-	subprocess.call(['glib-compile-schemas', schemadir])
+	subprocess.call(['glib-compile-schemas', schemadir], shell=False)
