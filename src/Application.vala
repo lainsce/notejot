lainsce/notejot

commit 6f53d71ffa5d2fcc5a0c7c782954bca79da638a7

* - Some suggestions made

src/Application.vala
@@ -17,44 +17,28 @@
 * Boston, MA 02110-1301 USA
 *
 */
-namespace Notejot {
-    public class Application : Adw.Application {
-        public static MainWindow win = null;
-        public static Settings gsettings;
-        private const GLib.ActionEntry app_entries[] = {
-            { "quit", on_quit },
-        };
+public class Notejot.Application : Adw.Application {
+    private const GLib.ActionEntry app_entries[] = {
+        { "quit", quit },
+    };
 
-        public Application () {
-            Object (
-                flags: ApplicationFlags.FLAGS_NONE,
-                application_id: Config.APP_ID
-            );
-            add_action_entries(app_entries, this);
-            set_resource_base_path ("/io/github/lainsce/Notejot/");
-        }
-        static construct {
-            gsettings = new Settings ();
-        }
+    public Application () {
+        Object (application_id: Config.APP_ID);
+    }
+    public static int main (string[] args) {
+        var app = new Notejot.Application ();
+        return app.run (args);
+    }
+    protected override void startup () {
+        resource_base_path = "/io/github/lainsce/Notejot";
 
-        construct {
-            Intl.setlocale (LocaleCategory.ALL, "");
-            Intl.bindtextdomain (Config.GETTEXT_PACKAGE, Config.GNOMELOCALEDIR);
-            Intl.textdomain (Config.GETTEXT_PACKAGE);
-        }
-        private void on_quit() {
-            win.destroy();
-        }
-        protected override void activate () {
-            if (win != null) {
-                win.present ();
-                return;
-            }
-            win = new MainWindow (this);
-        }
-        public static int main (string[] args) {
-            var app = new Notejot.Application ();
-            return app.run (args);
-        }
+        base.startup ();
+
+        add_action_entries (app_entries, this);
+
+        new MainWindow (this);
+    }
+    protected override void activate () {
+        active_window?.present ();
     }
 }

data/style.css
@@ -106,8 +106,7 @@
 }
 
 .content-sidebar {
-	margin: 6px;
-	margin-bottom: 0px;
+	margin: 0 6px;
 	border-radius: 6px;
 	border: none;
 	background: @popover_bg_color;
@@ -160,8 +159,16 @@
 	background: none;
 }
 
-.pan-down {
-	transform: scale(0.67);
+.notejot-theme-grid {
+  margin: 6px;
+}
+
+.notejot-searchbar {
+  border-radius: 999px;
+  padding: 0 18px;
+  margin: 6px 0;
+  border: none;
+  background: @popover_bg_color;
 }
 
 .nw-action {
@@ -201,27 +208,12 @@
   color: @view_bg_color;
 }
 
-.notejot-theme-grid {
-  margin: 6px;
-}
-
-.notejot-searchbar {
-  border-radius: 6px;
-  border: none;
-  margin: 0 6px;
-  margin-bottom: 6px;
-  padding: 4px 12px;
-  background: @popover_bg_color;
-}
-
 .big-font {
   font-size: 1.4rem;
 }
-
 .med-font {
   font-size: 1.1rem;
 }
-
 .sml-font {
   font-size: 0.9rem;
 }

data/ui/main_window.ui
@@ -34,8 +34,18 @@
                         <child type="title">
                           <object class="GtkBox">
                             <child>
-                            <placeholder />
+                              <object class="GtkLabel">
+                                <property name="label">Notejot</property>
+                                <style>
+                                  <class name="title"/>
+                                </style>
+                              </object>
                             </child>
+                          </object>
+                        </child>
+                        <child type="end">
+                          <object class="GtkMenuButton" id="menu_button">
+                            <property name="icon-name">open-menu-symbolic</property>
                           </object>
                         </child>
                         <child>
@@ -135,10 +145,6 @@
                     <style>
                       <class name="notejot-stitle"/>
                     </style>
-                    <child type="title">
-                      <object class="GtkBox">
-                      </object>
-                    </child>
                     <child>
                       <object class="GtkButton" id="back_button2">
                         <property name="visible" bind-source="leaflet" bind-property="folded" bind-flags="sync-create"/>
@@ -149,29 +155,16 @@
                     <child type="title">
                       <object class="GtkBox">
                         <child>
-                          <object class="GtkLabel">
-                            <property name="label">Notejot</property>
+                          <object class="GtkSearchEntry" id="note_search">
+                            <property name="hexpand">1</property>
+                            <property name="placeholder-text" translatable="yes">Search for notes</property>
                             <style>
-                              <class name="title"/>
+                              <class name="notejot-searchbar"/>
                             </style>
                           </object>
                         </child>
                       </object>
                     </child>
-                    <child type="end">
-                      <object class="GtkMenuButton" id="menu_button">
-                        <property name="icon-name">open-menu-symbolic</property>
-                      </object>
-                    </child>
-                  </object>
-                </child>
-                <child>
-                  <object class="GtkSearchEntry" id="note_search">
-                    <property name="hexpand">1</property>
-                    <property name="placeholder-text" translatable="yes">Search for notes</property>
-                    <style>
-                      <class name="notejot-searchbar"/>
-                    </style>
                   </object>
                 </child>
                 <child>
@@ -288,8 +281,8 @@
                                 <property name="action-name">win.action_trash_notes</property>
                                 <property name="valign">end</property>
                                 <property name="halign">end</property>
-                                <property name="margin-end">12</property>
-                                <property name="margin-bottom">6</property>
+                                <property name="margin-end">18</property>
+                                <property name="margin-bottom">12</property>
                                 <style>
                                   <class name="list-action-button"/>
                                   <class name="destructive-action"/>

src/MainWindow.vala
@@ -137,10 +137,6 @@ namespace Notejot {
         }
 
         construct {
-            // Dark theme
-            var adwsm = Adw.StyleManager.get_default ();
-            adwsm.set_color_scheme(Adw.ColorScheme.PREFER_LIGHT);
-
             // Actions
             actions = new SimpleActionGroup ();
             actions.add_action_entries (ACTION_ENTRIES, this);
@@ -160,9 +156,6 @@ namespace Notejot {
             app.set_accels_for_action ("win.action_italic", {"<Ctrl>i"});
             app.set_accels_for_action ("win.action_ul", {"<Ctrl>u"});
             app.set_accels_for_action ("win.action_s", {"<Ctrl><Shift>s"});
-
-            var action_fontsize = Notejot.Application.gsettings.create_action ("font-size");
-            app.add_action(action_fontsize);
 
             // Main View
             tm = new TaskManager (this);
@@ -250,13 +243,16 @@ namespace Notejot {
             });
 
             // Preparing window to be shown
+            var settings = new Settings ();
             set_default_size(
-                Application.gsettings.get_int ("window-w"),
-                Application.gsettings.get_int ("window-h")
+                settings.window_w,
+                settings.window_h
             );
-
-            if (Application.gsettings.get_boolean("is-maximized"))
+            if (settings.is_maximized)
                 maximize ();
+
+            var action_fontsize = settings.create_action ("font-size");
+            app.add_action(action_fontsize);
 
             this.show ();
 
@@ -265,12 +261,12 @@ namespace Notejot {
 
         protected override bool close_request () {
             debug ("Exiting window... Disposing of stuff...");
-
-            Application.gsettings.set_boolean("is-maximized", is_maximized ());
+            var settings = new Settings ();
+            settings.is_maximized = is_maximized ();
 
             if (!is_maximized()) {
-                Application.gsettings.set_int("window-w", get_width ());
-                Application.gsettings.set_int("window-h", get_height ());
+                settings.window_w = get_width ();
+                settings.window_h = get_height ();
             }
 
             this.dispose ();
@@ -428,7 +424,8 @@ namespace Notejot {
 
         public void action_all_notes () {
             sidebar_stack.set_visible_child (list_scroller);
-            Notejot.Application.gsettings.set_string("last-view", "list");
+            var settings = new Settings ();
+            settings.last_view = "list";
             main_stack.set_visible_child (empty_state);
             format_revealer.set_reveal_child (false);
             leaflet.set_visible_child (sgrid);
@@ -453,7 +450,8 @@ namespace Notejot {
 
         public void action_trash () {
             sidebar_stack.set_visible_child (trash_scroller);
-            Notejot.Application.gsettings.set_string("last-view", "trash");
+            var settings = new Settings ();
+            settings.last_view = "trash";
             main_stack.set_visible_child (empty_state);
             format_revealer.set_reveal_child (false);
             leaflet.set_visible_child (sgrid);

src/Settings.vala
@@ -1,20 +1,20 @@
-namespace Notejot {
-    public class Settings : GLib.Settings {
-        public bool is_maximized { get; set; }
-        public string last_view { get; set; }
-        public string font_size { get; set; }
-        public int window_w { get; set; }
-        public int window_h { get; set; }
-    
-        public Settings () {
-            Object (
-                schema_id: "io.github.lainsce.Notejot"
-            );
+[SingleInstance]
+public class Notejot.Settings : Object {
+    private GLib.Settings settings = new GLib.Settings ("io.github.lainsce.Notejot");
+    public bool is_maximized { get; set; }
+    public string last_view { get; set; }
+    public string font_size { get; set; }
+    public int window_w { get; set; }
+    public int window_h { get; set; }
 
-            bind ("last-view", this, "last-view", GLib.SettingsBindFlags.DEFAULT);
-            bind ("font-size", this, "font-size", GLib.SettingsBindFlags.DEFAULT);
-            bind ("window-w", this, "window-w", GLib.SettingsBindFlags.DEFAULT);
-            bind ("window-h", this, "window-h", GLib.SettingsBindFlags.DEFAULT);
-        }
+    construct {
+        settings.bind ("last-view", this, "last-view", DEFAULT);
+        settings.bind ("font-size", this, "font-size", DEFAULT);
+        settings.bind ("window-w", this, "window-w", DEFAULT);
+        settings.bind ("window-h", this, "window-h", DEFAULT);
+    }
+
+    public Action create_action (string key) {
+        return settings.create_action (key);
     }
 }

src/Widgets/TextField.vala
@@ -27,7 +27,6 @@ namespace Notejot {
             this.left_margin = this.right_margin = this.top_margin = this.bottom_margin = 30;
             this.wrap_mode = Gtk.WrapMode.WORD;
 
-
             var buffer = new Gtk.TextBuffer (null);
             this.buffer = buffer;
             set_buffer (buffer);
@@ -42,12 +41,10 @@ namespace Notejot {
             ul_font = buffer.create_tag("underline", "underline", Pango.Underline.SINGLE);
             s_font = buffer.create_tag("strike", "strikethrough", true);
 
+            var settings = new Settings ();
             set_font_stylesheet ();
+            settings.notify["font-size"].connect (set_font_stylesheet);
             fmt_syntax_start ();
-
-            Notejot.Application.gsettings.changed.connect (() => {
-                set_font_stylesheet ();
-            });
 
             buffer.changed.connect (() => {
                 send_text ();
@@ -83,22 +80,25 @@ namespace Notejot {
         }
 
         private void set_font_stylesheet () {
-            if (Notejot.Application.gsettings.get_string("font-size") == "'small'") {
-                this.get_style_context ().add_class ("sml-font");
-                this.get_style_context ().remove_class ("med-font");
-                this.get_style_context ().remove_class ("big-font");
-            } else if (Notejot.Application.gsettings.get_string("font-size") == "'medium'") {
-                this.get_style_context ().remove_class ("sml-font");
-                this.get_style_context ().add_class ("med-font");
-                this.get_style_context ().remove_class ("big-font");
-            } else if (Notejot.Application.gsettings.get_string("font-size") == "'large'") {
-                this.get_style_context ().remove_class ("sml-font");
-                this.get_style_context ().remove_class ("med-font");
-                this.get_style_context ().add_class ("big-font");
-            } else {
-                this.get_style_context ().remove_class ("sml-font");
-                this.get_style_context ().add_class ("med-font");
-                this.get_style_context ().remove_class ("big-font");
+            var settings = new Settings ();
+
+            switch (settings.font_size) {
+                case "'small'":
+                    add_css_class ("sml-font");
+                    remove_css_class ("med-font");
+                    remove_css_class ("big-font");
+                    break;
+                default:
+                case "'medium'":
+                    remove_css_class ("sml-font");
+                    add_css_class ("med-font");
+                    remove_css_class ("big-font");
+                    break;
+                case "'large'":
+                    remove_css_class ("sml-font");
+                    remove_css_class ("med-font");
+                    add_css_class ("big-font");
+                    break;
             }
         }
 

commit b5898b85821999032152464ec7b088c1ff8e9b88

Initialize translations properly
Solves regression introduced in 6f53d71ffa5d2fcc5a0c7c782954bca79da638a7
and improves faeaff5d9a113d022f6405eb1a217770e2395d5c implementation.

Part of https://github.com/lainsce/notejot/issues/313

src/Application.vala
@@ -26,6 +26,9 @@ public class Notejot.Application : Adw.Application {
         Object (application_id: Config.APP_ID);
     }
     public static int main (string[] args) {
+        Intl.bindtextdomain (Config.GETTEXT_PACKAGE, Config.GNOMELOCALEDIR);
+        Intl.textdomain (Config.GETTEXT_PACKAGE);
+
         var app = new Notejot.Application ();
         return app.run (args);
     }